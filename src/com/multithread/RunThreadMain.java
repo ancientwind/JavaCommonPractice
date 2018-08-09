2e97f2258073ff3e79e0a95564f16283b17c6241
zengqw <212331901@logon.ds.ge.com>
2018-08-09 15:03:39 +0800

    swing ui unresponsive

--- src/com/multithread/RunThreadMain.java
+++ src/com/multithread/RunThreadMain.java
@@ -7,7 +7,7 @@ public class RunThreadMain {
 	public static void main(String[] args) {
 		
 		RunThreadMain main = new RunThreadMain();
-//		main.runRunnableDemo();
+		main.runRunnableDemo();
 		Double a = 3.51212;
 		Double b = 1004444.032434234;
 		Double result;
